528c0555fbf6ae7dc59fd2f93214969a915b3fb8
shrivastavarahul <rhshriva@gmail.com>
2015-05-19 11:33:18 -0700

    Initial checkin

---
+++ src/main/java/com/simplelogger/Main.java
@@ -0,0 +1,11 @@
+package com.simplelogger;
+
+/**
+ * Created by rahul on 5/19/15.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println(" hello world ");
+    }
+}
